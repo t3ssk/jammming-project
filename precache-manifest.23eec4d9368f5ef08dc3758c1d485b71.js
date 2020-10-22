381935440c4b28153a0c25fed9338ec9776d5874
t3ssk <te.konecna@icloud.com>
2020-10-22 20:23:03 +0200

    Updates

---
+++ precache-manifest.23eec4d9368f5ef08dc3758c1d485b71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80503de899b5ead95ed17816925d80f4",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f32bdaa03b798c03396c",
+    "url": "/my-app/static/css/main.0a3eff25.chunk.css"
+  },
+  {
+    "revision": "c354c3055c4ef22e36d6",
+    "url": "/my-app/static/js/2.2c76274e.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/my-app/static/js/2.2c76274e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f32bdaa03b798c03396c",
+    "url": "/my-app/static/js/main.9f3946a2.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "4ac1a9649ba7bb1aa2adeec7a8254bb9",
+    "url": "/my-app/static/media/eberhard-grossgasteiger-NvesrDbsrL4-unsplash.4ac1a964.jpg"
+  }
+]);
