a4eb0ce95c5931383b16f3e39e5725b1a96fe2fa
t3ssk <te.konecna@icloud.com>
2020-10-22 20:45:05 +0200

    Updates

---
+++ precache-manifest.205c419aaae9fb8439ffc914c7b8e645.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0743da1a21cf47240843c68419e5960a",
+    "url": "/jammming-project/index.html"
+  },
+  {
+    "revision": "0b1cc5d55eabac17dccb",
+    "url": "/jammming-project/static/css/main.09d9fb3e.chunk.css"
+  },
+  {
+    "revision": "92797116e4dc1d254384",
+    "url": "/jammming-project/static/js/2.988083f1.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/jammming-project/static/js/2.988083f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b1cc5d55eabac17dccb",
+    "url": "/jammming-project/static/js/main.333e0554.chunk.js"
+  },
+  {
+    "revision": "94a277a7b13ab856e1e3",
+    "url": "/jammming-project/static/js/runtime-main.d8fa9ec3.js"
+  },
+  {
+    "revision": "4ac1a9649ba7bb1aa2adeec7a8254bb9",
+    "url": "/jammming-project/static/media/eberhard-grossgasteiger-NvesrDbsrL4-unsplash.4ac1a964.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.4cfcbe118e3ab7a10a1f437883a3ba37.js"
+  "/jammming-project/precache-manifest.205c419aaae9fb8439ffc914c7b8e645.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jammming-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
